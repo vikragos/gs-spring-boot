d9e542b00943b561491ca66a0125fa274c090435
vikragos <vikragos@gmail.com>
2020-11-24 20:36:03 -0600

    Update HelloController.java

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Srini!";
+        return "Hello from Spring Boot!";
     }   
 }
